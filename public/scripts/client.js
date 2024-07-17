50d44f2a8b884255190114a8b8cfeea66ea4eccb
Sampson Chang <sampson.chang@gmail.com>
2024-07-17 15:34:04 -0700

    added error when tweet are too long or no input

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
   const createTweetElement = function(tweetData) {
     const {name, avatars, handle} = tweetData.user;
     const body = tweetData.content.text;
@@ -90,9 +66,14 @@ $(document).ready(function() {
       $('#tweets-container').prepend($tweet);
     }
   }
-  renderTweets(data);
   const loadTweets = function() {
-    $.get('http://localhost:8080/tweets', renderTweets)
+    $.get('http://localhost:8080/tweets')
+      .done((data) => {
+        renderTweets(data);
+      })
+      .fail((error) => {
+        console.error(error);
+      });
   }
   loadTweets();
 })
--- public/scripts/submit.js
+++ public/scripts/submit.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
   $(".tweeting").on('submit', function(event) {
     event.preventDefault();
     const data = $(this).serialize();
-    console.log(data)
+    const count =$("#tweet-text").val().length;
+    console.log(count)
+    if(count > 140){
+      return alert("Too many words!")
+    } else if (count === 0) {
+      return alert("no input!")
+    }
     $.ajax({
       type: "POST",
       url: '/tweets',
