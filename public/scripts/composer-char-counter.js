eb4c2bc005d74bea5b74e097d365b188b8eff55f
Sampson Chang <sampson.chang@gmail.com>
2024-07-16 22:26:47 -0700

    added a tweet box

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $("#tweet-text").on("input", function() {
+    const charCount = $(this).val().length;
+    const value =$("output.counter");
+    value.text(140 - charCount);
+    let overValue = 140 - charCount;
+    if(overValue < 0){
+      value.css('color', 'red');
+    } else {
+      value.css('color', "");
+    }
+  })
+});
+
