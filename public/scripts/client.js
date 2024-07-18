7abf4ce71032c7f01106266b8e133bdd235dc071
Sampson Chang <sampson.chang@gmail.com>
2024-07-17 17:51:54 -0700

    auto refresh after submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -76,4 +76,23 @@ $(document).ready(function() {
       });
   }
   loadTweets();
+
+  $(".tweeting").on('submit', function(event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+    const count =$("#tweet-text").val().trim().length;
+    
+    if(count > 140){
+      return alert("Too many words!")
+    } else if (count === 0) {
+      return alert("no input!")
+    }
+    $.ajax({
+      type: "POST",
+      url: '/tweets',
+      data: data
+    })
+    .then(loadTweets);
+    $(".tweeting")[0].reset();
+  })
 })
--- public/scripts/submit.js
+++ public/scripts/submit.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
-  $(".tweeting").on('submit', function(event) {
-    event.preventDefault();
-    const data = $(this).serialize();
-    const count =$("#tweet-text").val().length;
-    console.log(count)
-    if(count > 140){
-      return alert("Too many words!")
-    } else if (count === 0) {
-      return alert("no input!")
-    }
-    $.ajax({
-      type: "POST",
-      url: '/tweets',
-      data: data
-    });
-  })
-});
+// $(document).ready(function() {
+//   $(".tweeting").on('submit', function(event) {
+//     event.preventDefault();
+//     const data = $(this).serialize();
+//     const count =$("#tweet-text").val().trim().length;
+    
+//     if(count > 140){
+//       return alert("Too many words!")
+//     } else if (count === 0) {
+//       return alert("no input!")
+//     }
+//     $.ajax({
+//       type: "POST",
+//       url: '/tweets',
+//       data: data
+//     });
+//   })
+// });
