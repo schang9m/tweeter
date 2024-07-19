821ae56ed7f1025d7a4da827f23732edd64fcfd4
Sampson Chang <sampson.chang@gmail.com>
2024-07-18 23:33:03 -0700

    fix syntax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,12 +19,12 @@ $(document).ready(function() {
     const $handle = $('<p>').text(handle);
     const $divBody = $('<div><p>').text(body);
     const $footer = $('<footer>');
-    const $date = $('<time>').text(`${date}`)
-    const $icon = $('<div>')
-    $($icon).attr('id', 'interact')
-    $icon.append('<i class="fa-solid fa-flag">')
-    $icon.append('<i class="fa-solid fa-retweet">')
-    $icon.append('<i class="fa-solid fa-heart">')
+    const $date = $('<time>').text(`${date}`);
+    const $icon = $('<div>');
+    $($icon).attr('id', 'interact');
+    $icon.append('<i class="fa-solid fa-flag">');
+    $icon.append('<i class="fa-solid fa-retweet">');
+    $icon.append('<i class="fa-solid fa-heart">');
     $article.append($header);
     $header.append($div);
     $header.append($handle);
@@ -55,7 +55,7 @@ $(document).ready(function() {
     //       </footer>
     //     </article>`);
     // return $tweet;
-  }
+  };
 
   const renderTweets = function(tweets) {
     // loops through tweets
@@ -65,7 +65,7 @@ $(document).ready(function() {
       const $tweet = createTweetElement(tweetData);
       $('#tweets-container').prepend($tweet);
     }
-  }
+  };
   const loadTweets = function() {
     $.get('http://localhost:8080/tweets')
       .done((data) => {
@@ -74,85 +74,91 @@ $(document).ready(function() {
       .fail((error) => {
         console.error(error);
       });
-  }
+  };
   loadTweets();
   //reseting the textbox
   const reset = function() {
     $(".tweeting")[0].reset();
     $("output.counter").text(140);
     $("output.counter").css('color', "");
-  }
+  };
   //slide new tweet up and down
-  $(".writeTweet").on("click", function(){
-    $(".tweeting").slideToggle( "slow", function(){
+  $(".writeTweet").on("click", function() {
+    $(".tweeting").slideToggle("slow", function() {
       //auto focus
       $('#tweet-text').focus();
     });
   });
-  //auto submit when press enter 
-  $("#tweet-text").on("keypress", function(event){
-    if (event.key === "Enter"){
+  //auto submit when press enter
+  $("#tweet-text").on("keypress", function(event) {
+    if (event.key === "Enter") {
       event.preventDefault();
       $(".tweeting").submit();
     }
-  })
+  });
 
   $(".tweeting").on('submit', function(event) {
     event.preventDefault();
     const data = $(this).serialize();
-    const count =$("#tweet-text").val().trim().length;
+    const count = $("#tweet-text").val().trim().length;
     
     if (count > 140) {
       $("#error-messages").text("❌Too Many Words Yo, 140 Limit Pls❌").slideDown(400);
       reset();
-      return $("#error-messages").slideUp(3500, function() {$(this).empty()}); 
+      return $("#error-messages").slideUp(3500, function() {
+        $(this).empty();
+      });
     } else if (count === 0) {
       $("#error-messages").text("❌You Need To Enter Something?!❌").slideDown(400);
-      reset(); 
-      return $("#error-messages").slideUp(1500, function() {$(this).empty()}); 
+      reset();
+      return $("#error-messages").slideUp(1500, function() {
+        $(this).empty();
+      });
     } else {
       $.ajax({
         type: "POST",
         url: '/tweets',
         data: data,
         success: function() {
-            $("#tweets-container").empty();
-            $(".tweeting")[0].reset();
-            loadTweets();
+          $("#tweets-container").empty();
+          $(".tweeting")[0].reset();
+          loadTweets();
         },
         error: function(xhr, status, error) {
-            // Handle the error here
-            $("#error-messages").text(`❌AJAX request failed:${status},${error}❌`).slideDown(400);
-            reset(); 
-            return $("#error-messages").slideUp(1500, function() {$(this).empty()}); 
-            // You can display an error message to the user or take other actions
+          // Handle the error here
+          $("#error-messages").text(`❌AJAX request failed:${status},${error}❌`).slideDown(400);
+          reset();
+          return $("#error-messages").slideUp(1500, function() {
+            $(this).empty();
+          });
+          // You can display an error message to the user or take other actions
         }
-    });
+      });
     }
-  })
+  });
   //move the icon down
   $("nav .writeTweet").hover(function() {
     $('nav .fa-solid').stop().animate({top:10});
-}, function() {
+  }, function() {
     $('nav .fa-solid').stop().animate({top:0});
-});
-//top button shows up and gone as you scroll
-$(window).on('scroll', function() {
-  if ($(this).scrollTop() > 100) {
-    $("#topBtn").fadeIn();
-    $("nav").fadeOut();
-  } else {
-    $("#topBtn").fadeOut();
-    $("nav").fadeIn();
+  });
+  //top button shows up and gone as you scroll
+  $(window).on('scroll', function() {
+    if ($(this).scrollTop() > 100) {
+      $("#topBtn").fadeIn();
+      $("nav").fadeOut();
+    } else {
+      $("#topBtn").fadeOut();
+      $("nav").fadeIn();
 
-  }
-});
-//clicking on the top button
-$("#topBtn").on('click', function(e) {
-  $("html, body").animate({scrollTop: 0}, 500);
-  $(".tweeting").slideToggle( "slow", function(){
+    }
+  });
+  //clicking on the top button
+  $("#topBtn").on('click', function(e) {
+    $("html, body").animate({scrollTop: 0}, 500);
+    $(".tweeting").slideToggle("slow", function() {
     //auto focus
-    $('#tweet-text').focus();
-  })
+      $('#tweet-text').focus();
+    });
+  });
 });
-})
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
   // --- our code goes here ---
   $("#tweet-text").on("input", function() {
     const charCount = $(this).val().length;
-    const value =$("output.counter");
+    const value = $("output.counter");
     value.text(140 - charCount);
     let overValue = 140 - charCount;
-    if(overValue < 0){
+    if (overValue < 0) {
       value.css('color', 'red');
     } else {
       value.css('color', "");
     }
-  })
+  });
 });
